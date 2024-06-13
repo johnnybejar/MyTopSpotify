4c40cbffa984b6e68a9bb5dd9608ed2db513c313
johnnyb <jbejar2001@yahoo.com>
2024-06-13 17:19:58 -0400

    basic frontend + reorganized css files + components folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
-import './App.css';
+import Header from './components/Header';
+import "./styles/App.css";
 
 function App() {
   return (
     <div className='main'>
+      <Header />
       <div className="autheticate" >
         Click here to connect your spotify account!
       </div>
---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import "../styles/Header.css"
+
+function Header() {
+  return (
+    <div className="header">
+        <span className="title">Spotify Visualizer</span>
+    </div>
+  );
+}
+
+export default Header;
