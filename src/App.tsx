073f904038d34ee594dc4f327ec539d7baddc97d
johnnyb <jbejar2001@yahoo.com>
2024-06-14 15:00:40 -0400

    added spotify logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import Header from './components/Header';
+import spotify from "./public/spotify-logo-text.png";
 import "./styles/App.css";
 
 function App() {
@@ -6,7 +7,7 @@ function App() {
     <div className='main'>
       <Header />
       <div className="autheticate" >
-        Click here to connect your spotify account!
+        Click here to connect your <img src={spotify} className='spotify-logo' alt="Spotify" /> account!
       </div>
     </div>
   );
