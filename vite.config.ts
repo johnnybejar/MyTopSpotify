7019d62382372e2d5ce3d735527ebc4701e0cd2d
johnnyb <jbejar2001@yahoo.com>
2024-06-19 17:03:52 -0400

    removed proxy + tanstack

--- vite.config.ts
+++ vite.config.ts
@@ -3,19 +3,5 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      "/auth/token": {
-        target: "localhost:5000",
-        changeOrigin: true,
-        secure: false,
-      },
-      "/auth/login": {
-        target: "localhost:5000",
-        changeOrigin: true,
-        secure: false,
-      }
-    }
-  }
+  plugins: [react()]
 });
