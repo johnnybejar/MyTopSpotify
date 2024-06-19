527021700756774b86370131c7922af7c67ac3e5
johnnyb <jbejar2001@yahoo.com>
2024-06-18 20:44:28 -0400

    more implementation to auth pkce

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -19,29 +19,80 @@ async function generateCodeChallenge(plainData: string) {
         .replace(/\//g, '_');
 }
 
-const codeVerifier  = generateRandomString(64);
-const codeChallenge = generateCodeChallenge(codeVerifier).then(val => {return val});
+function createAuthorizationUri() {
+    const codeVerifier  = generateRandomString(64);
+    const codeChallengePromise = generateCodeChallenge(codeVerifier);
+    let codeChallenge = "";
+    codeChallengePromise.then(val => {codeChallenge = val})
+    const state = generateRandomString(16);
 
-const CLIENT_ID = import.meta.env.CLIENT_ID;
-const redirectUri = "http://localhost:5173";
+    localStorage.setItem("CodeVerifier", codeVerifier);
+    localStorage.setItem("State", state);
 
-const scope = "user-read-private user-read-email user-read-currently-playing user-read-recently-played user-top-read playlist-read-collaborative";
-const authUrl = new URL("https://accounts.spotify.com/authorize");
+    const CLIENT_ID = import.meta.env.CLIENT_ID;
+    const redirectUri = "http://localhost:5173";
 
-window.localStorage.setItem("code_verifier", codeVerifier);
+    const scope = "user-read-private user-read-email user-read-currently-playing user-read-recently-played user-top-read playlist-read-collaborative";
+    const authUrl = new URL("https://accounts.spotify.com/authorize");
 
-if (!codeChallenge) {
-    throw Error();
+    const params = {
+        response_type: "code",
+        client_id: CLIENT_ID,
+        scope,
+        code_challenge_method: "S256",
+        code_challenge: codeChallenge,
+        redirect_uri: redirectUri
+    } as Record<string, string>;
+
+    authUrl.search = new URLSearchParams(params).toString();
+    window.location.href = authUrl.toString(); 
 }
 
-const params = {
-    response_type: "code",
-    client_id: CLIENT_ID,
-    scope,
-    code_challenge_method: "S256",
-    code_challenge: codeChallenge,
-    redirect_uri: redirectUri
+async function generateToken(state: string, code: string) {
+    const CLIENT_ID = import.meta.env.CLIENT_ID;
+    const savedState = localStorage.getItem("State");
+    localStorage.removeItem("State");
+
+    if (state !== savedState) {
+        return { error: true };
+    }
+
+    const postConfig = {
+        headers: {
+            'content-type': 'application/x-www-form-urlencoded'
+        }
+    };
+
+    const parameters = {
+        client_id: CLIENT_ID,
+        grant_type: 'authorization_code',
+        code,
+        redirect_uri: "http://localhost:5173",
+        code_verifier: localStorage.getItem("CodeVerifier")
+    }
+
+    try {
+        
+    }
+}
+
+function saveToken(tokenData: Record<string, string>) {
+    const { accessToken, expiresIn, refreshToken } = tokenData;
+
+    localStorage.setItem("AccessTokenKey", accessToken);
+    localStorage.setItem("AccessTokenExpiresIn", expiresIn);
+    localStorage.setItem("AccessTokenRefreshKey", refreshToken);
+}
+
+function getToken() {
+    return localStorage.getItem("AccessTokenKey");
+}
+
+const Auth = {
+    createAuthorizationUri,
+    generateToken,
+    saveToken,
+    getToken
 };
 
-authUrl.search = new URLSearchParams(params).toString();
-window.location.href = authUrl.toString();
+export default Auth;
