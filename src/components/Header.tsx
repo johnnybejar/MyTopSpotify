87b4d80d9eb3f2ccf9546d0612374854188a7807
johnnyb <jbejar2001@yahoo.com>
2024-06-25 21:37:34 -0400

    added logout to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,18 @@
 import "../styles/Header.css";
+import logout from "../public/logout.png";
+
 
 function Header() {
+  const logOut = () => {
+    localStorage.clear();
+    // TODO: Reload the component properly
+    window.location.reload();
+  }
+
   return (
     <div className="header">
         <a className="title" href="/">Spotify Visualizer</a>
+        <img src={logout} className="logout" onClick={logOut} alt="Logout" />
     </div>
   );
 }
