b9070c37995ea1c656bc74b64575cf33c012d163
johnnyb <jbejar2001@yahoo.com>
2024-07-15 12:09:21 -0400

    added spotify icon to playback

--- src/components/Playback.tsx
+++ src/components/Playback.tsx
@@ -4,6 +4,7 @@ import { BounceLoader } from "react-spinners";
 import { useToken } from "../context/TokenProvider";
 import { toast } from "react-toastify";
 import Auth from "../services/auth";
+import SpotifyLogo from "../public/spotify-icon.png"
 import "../styles/Playback.css";
 
 function Playback() {
@@ -49,16 +50,24 @@ function Playback() {
     }
 
     if (error) {
-        return <span className="error">Cannot display playback state due to an error, try re-authenticating or try again later</span>
+        return <span className="error">Cannot display playback state due to an error, try re-authenticating or try again later</span>;
     }
 
     if (Object.keys(playback).length === 0) {
-        return <span className="playback-not-listening">Currently not playing anything</span>
+        return (
+            <div className="playback-header">
+                <img src={SpotifyLogo} alt="spotify icon" className="playback-spotify-img" />
+                <span className="playback-not-listening">Currently not playing anything</span>
+            </div>
+        );
     }
 
     return (
         <div className="playback">
-            <span className="playback-listening">Currently listenting to:</span>
+            <div className="playback-header">
+                <img src={SpotifyLogo} alt="spotify icon" className="playback-spotify-img" />
+                <span className="playback-listening">Currently listenting to:</span>
+            </div>
             <div className="playback-track">
                 <a href={playback.item?.album.external_urls.spotify} className="playback-image-link" target="_blank">
                     <img src={playback.item?.album.images[0].url} className="playback-image" alt="album-cover" />
